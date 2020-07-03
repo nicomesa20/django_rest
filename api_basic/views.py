136971f19138ac1ec5ede9c291de2466a952465e
nicomesa20 <nicomesa2013@gmail.com>
2020-07-03 02:06:18 -0500

    Working class based functions

--- api_basic/views.py
+++ api_basic/views.py
@@ -5,9 +5,53 @@ from .models import Article
 from .serializers import ArticleSerializer
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
-from rest_framework.response import Response 
+from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 # Create your views here.
+
+
+class ArticleAPIView(APIView):
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ArticleDetails(APIView):
+    def get_object(self, pk):
+        try:
+            article = Article.objects.get(pk=pk)
+
+        except Article.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self,request,pk):
+        article = self.get_object(pk)
+        print(article)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    
+    def put(self,request,id):
+        article = self.get_object
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self,request):
+        article = self.get_object
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET', 'POST'])
 def article_list(request):
@@ -25,7 +69,8 @@ def article_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET','PUT','DELETE'])
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def article_detail(request, pk):
     try:
         article = Article.objects.get(pk=pk)
@@ -34,7 +79,7 @@ def article_detail(request, pk):
 
     if request.method == 'GET':
         serializer = ArticleSerializer(article)
-        return Response(serializer.data)    
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
         serializer = ArticleSerializer(article, data=request.data)
